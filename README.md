193eb1b027fbe874b32c7b798f213eec7f7b52f7
Zee-2000 <zahwaabdelnasser@gmail.com>
2025-04-06 00:34:26 +0200

    just the beginning

---
+++ README.md
@@ -0,0 +1,7 @@
+this is my first real life project that can improve my skills in programming
+My first step in applying on the rea life projects 
+
+In this project I'm buliding my own wc command line in Unix, the command is called ccwc, which is part of Coding challanges track.
+
+this peogram will be made by C language which is suitable for building this command line 
+
---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#define macro 
+// this project is for making ccwc command line 
+//My own wc tool 
+
+/// @brief a function that counts bytes
+/// @param  bytes : bytes counted in the text file 
+/// @param command_str : 
+/// @return number of bytes in a file
+
+int countBytes(bytes, command_str)
+{
+    /*char filename[] = "test.txt";
+    for (size_t i = 0; i < count; i++)
+    {
+        /* code */
+    }
+    
+    /*scanf("%s", &bytes);
+    printf(" %d", bytes);
+    return 0;
+    */
+
+
+}
